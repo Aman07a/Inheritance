49f80ffe9f8e444526bc998c01a5e151275bbd22
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-07 10:31:04 +0100

    Inheritance Challenge 2 - Employees, Bosses and Trainees Solution

--- 09 - Inheritance/Program.cs
+++ 09 - Inheritance/Program.cs
@@ -24,37 +24,64 @@ namespace _09___Inheritance
             // dog.Play();
             // dog.Eat();
 
-            Post post1 = new Post("Thanks for the birthday wishes", true, "User");
-            Console.WriteLine(post1.ToString());
+            // Post post1 = new Post("Thanks for the birthday wishes", true, "User");
+            // Console.WriteLine(post1.ToString());
 
-            ImagePost imagePost1 = new ImagePost("Check out my GitHub", "User",
-                "https://github.com/Aman07a", true);
+            // ImagePost imagePost1 = new ImagePost("Check out my GitHub", "User",
+            //     "https://github.com/Aman07a", true);
 
-            VideoPost videoPost1 = new VideoPost("FailVideo", "User",
-                "https://www.youtube.com/results?search_query=failvideo", true, 10);
+            // VideoPost videoPost1 = new VideoPost("FailVideo", "User",
+            //     "https://www.youtube.com/results?search_query=failvideo", true, 10);
 
-            Console.WriteLine(imagePost1.ToString());
-            Console.WriteLine(videoPost1.ToString());
+            // Console.WriteLine(imagePost1.ToString());
+            // Console.WriteLine(videoPost1.ToString());
 
-            videoPost1.Play();
-            Console.WriteLine("Press any key to stop the video!");
-            Console.ReadKey();
-            videoPost1.Stop();
-            Console.ReadLine();
+            // videoPost1.Play();
+            // Console.WriteLine("Press any key to stop the video!");
+            // Console.ReadKey();
+            // videoPost1.Stop();
+            // Console.ReadLine();
+
+            // Employee michael = new Employee("Michael", "Miller", 40000);
+
+            // michael.Work();
+            // michael.Pause();
+
+            // Boss chuckNorris = new Boss("Ferrari", "Norris", "Chuck", 999999999);
+
+            // chuckNorris.Lead();
+
+            // Trainee michelle = new Trainee(32, 8, "Gartner", "Michelle", 10000);
+            // michelle.Learn();
+            // michelle.Work();
+            // Console.ReadKey();
 
             Console.Read();
         }
 
-        // CHALLENGE: Add a deriving class "VideoPost" with a property VideoURL, Length.
+        // // CHALLENGE: Add a deriving class "VideoPost" with a property VideoURL, Length.
 
-        // Create the required constructors to create a VideoPost
-        // Adjust the ToString() method accordingly
-        // Create an instance of VideoPost and use the ToString method on it
+        // // Create the required constructors to create a VideoPost
+        // // Adjust the ToString() method accordingly
+        // // Create an instance of VideoPost and use the ToString method on it
 
-        // More advanced - use Timer and a Callback method here (google it)
-        // Create fields as required
-        // Add member methods "Play" which should write the current duration of the video
-        // And "Stop" which should stop the "timer" and write "stopped at {0}" onto the console
-        // Play the video after creating the instance and stop it, when the user presses any key
+        // // More advanced - use Timer and a Callback method here (google it)
+        // // Create fields as required
+        // // Add member methods "Play" which should write the current duration of the video
+        // // And "Stop" which should stop the "timer" and write "stopped at {0}" onto the console
+        // // Play the video after creating the instance and stop it, when the user presses any key
+
+        // // CHALLENGE 2:
+        // // Create a main class with the Main method, then a base class employee with the properties:
+        // // Name, FirstName, Salary and the methods Work() and Pause().
+        // // Create a deriving class boss with the property CompanyCar and the method Lead().
+        // // Create another deriving class of employees - trainees with the properties WorkingHours 
+        // // and SchoolHours and a method Learn();
+        // // Override the methods Work() of the trainee class so that it indicates 
+        // // the working hours of the trainee.
+        // // Don't forget to create the constructors.
+        // // Create an object of each of the three classes (with arbitrary values) 
+        // // and call the methods, Lead() of Boss and Work() of Trainee.
+        // // Just print out the respective text, what the respective employees do.
     }
 }

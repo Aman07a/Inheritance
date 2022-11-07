f58464862a9087b440b09f30bb4924f3ec4721dc
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-07 10:16:24 +0100

    Inheritance Challenge - Videopost and Timer with Callback

--- 09 - Inheritance/Program.cs
+++ 09 - Inheritance/Program.cs
@@ -26,11 +26,35 @@ namespace _09___Inheritance
 
             Post post1 = new Post("Thanks for the birthday wishes", true, "User");
             Console.WriteLine(post1.ToString());
-            ImagePost imagePost1 = new ImagePost("Check out my GitHub", "User", "https://github.com/Aman07a", true);
+
+            ImagePost imagePost1 = new ImagePost("Check out my GitHub", "User",
+                "https://github.com/Aman07a", true);
+
+            VideoPost videoPost1 = new VideoPost("FailVideo", "User",
+                "https://www.youtube.com/results?search_query=failvideo", true, 10);
+
             Console.WriteLine(imagePost1.ToString());
+            Console.WriteLine(videoPost1.ToString());
+
+            videoPost1.Play();
+            Console.WriteLine("Press any key to stop the video!");
+            Console.ReadKey();
+            videoPost1.Stop();
             Console.ReadLine();
 
             Console.Read();
         }
+
+        // CHALLENGE: Add a deriving class "VideoPost" with a property VideoURL, Length.
+
+        // Create the required constructors to create a VideoPost
+        // Adjust the ToString() method accordingly
+        // Create an instance of VideoPost and use the ToString method on it
+
+        // More advanced - use Timer and a Callback method here (google it)
+        // Create fields as required
+        // Add member methods "Play" which should write the current duration of the video
+        // And "Stop" which should stop the "timer" and write "stopped at {0}" onto the console
+        // Play the video after creating the instance and stop it, when the user presses any key
     }
 }
